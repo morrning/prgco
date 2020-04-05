389510f5cac0f31e20dc2081d03bcbe445f46943
babak alizadeh <alizadeh.babak@gmail.com>
2020-04-05 20:35:02 +0430

    some progress

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 use App\Service as Service;
 
 class HomeController extends AbstractController
@@ -11,11 +19,55 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home", options={"expose" = true})
      */
-    public function home(Service\EntityMGR $entityMGR,Service\UserMGR $userMGR)
+    public function home(Request $request, Service\EntityMGR $entityMGR, Service\UserMGR $userMGR, LoggerInterface $logger,Service\LogMGR $logMGR)
     {
         $area = null;
         if($userMGR->isLogedIn())
             $area = $entityMGR->find('App:SysArea',$userMGR->currentPosition()->getDefaultArea());
+
+        $defaultData = ['message' => 'Type your message here'];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('username', TextType::class,['label'=>'نام کاربری','attr'=>['placeholder'=>'نام کاربری']])
+            ->add('password', PasswordType::class,['label'=>'کلمه عبور','attr'=>['placeholder'=>'کلمه عبور']])
+            ->add('submit', SubmitType::class,['label'=>'ورود به پورتال'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        $alert = null;
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            if($userMGR->login($data['username'],$data['password'],true))
+            {
+                $position = $entityMGR->findOneBy('App:SysPosition',['userID'=>$userMGR->currentUser()]);
+                if(is_null($position))
+                {
+                    $userMGR->logout();
+                    $alert = [['message'=>'هیچ پست سازمانی برای شما تعریف نشده است.','type'=>'danger']];
+                }
+                else{
+                    if(is_null($entityMGR->findOneBy('App:SysPosition',['userID'=>$userMGR->currentUser(),'isDefault'=>'1']))){
+                        $position->setIsDefault(1);
+                        $entityMGR->update($position);
+                    }
+                    if($userMGR->currentUser()->getSuspend())
+                    {
+                        $userMGR->logout();
+                        $alert = [['message'=>'حساب کاربری شما توسط مدیرسامانه مسدود شده است.در صورتی که فکر می‌کنید اشتباهی رخ داده لطفا با مدیر سامانه تماس بگیرید.','type'=>'danger']];
+                    }
+                    else{
+                        $logger->info('user ' . $data['username'] .' loged in.');
+                        $logMGR->addEvent('3gv5','ورود به سامانه',sprintf('کاربر با نام کاربری %s وارد سامانه شد.',$data['username']),'USERS',$request->getClientIp());
+                        return $this->redirectToRoute('home');
+                    }
+
+                }
+
+            }
+            else{
+                $logger->alert('login failor for user ' . $data['username'] );
+                $alert = [['message'=>'نام‌کاربری یا کلمه‌عبور اشتباه است.','type'=>'danger']];
+            }
+        }
 
         $projects = $entityMGR->getORM()->createQueryBuilder('p')
             ->select(['p.id','a.areaName','p.lastUpdate','p.Pprogress','p.Cprogress'])
@@ -41,7 +93,8 @@ class HomeController extends AbstractController
             'breadcrumb'=>true,
             'pn'=>$projectNames,
             'pp'=>$pprogress,
-            'cp'=>$cprogress
+            'cp'=>$cprogress,
+            'form'=>$form->createView()
         ]);
     }
 
